ebcd9de55794705c7a7e04bb777552eec06ba5d9
Doron Behar <doron.behar@gmail.com>
2019-07-10 13:07:39 +0300

    Configure special build params for some programs

--- config.nix
+++ config.nix
@@ -22,17 +22,58 @@
     awesome = pkgs.awesome.override {
       luaPackages = lua53Packages;
     };
-    luaPackages = pkgs.lua53Packages.override {
-      overrides = self: super: {
-        lua-lsp = pkgs.lua53Packages.lua-lsp.overrideAttrs (oldAttrs: rec {
-          src = fetchgit {
-            url = "https://github.com/doronbehar/lua-lsp.git";
-            rev = "ec1b6f0";
-            sha256 = "1mlgb2dp1ah76hjkbkwz0dp2y5mn491v5wf81nm086rb2fa3rzcg";
-          };
-        });
+    sxiv = pkgs.sxiv.overrideAttrs (oldAttrs: rec {
+      src = fetchgit {
+        url = "https://github.com/doronbehar/sxiv.git";
+        rev = "4c99b47";
+        sha256 = "1lc9y6d5qqi0sar211z55vmygibaal6bnwk7jj7336chwavxcbip";
       };
+    });
+    neovim = pkgs.neovim.override {
+      extraPython3Packages = (ps: with ps; [
+        simple-websocket-server
+        python-slugify
+      ]);
     };
+    lua-lsp = pkgs.lua53Packages.lua-lsp.overrideAttrs (oldAttrs: rec {
+      src = fetchgit {
+        url = "https://github.com/doronbehar/lua-lsp.git";
+        rev = "ec1b6f0";
+        sha256 = "1mlgb2dp1ah76hjkbkwz0dp2y5mn491v5wf81nm086rb2fa3rzcg";
+      };
+    });
+    octave = pkgs.octave.overrideAttrs (oldAttrs: rec {
+      buildInputs = [ gfortran readline ncurses perl flex texinfo qhull
+        graphicsmagick pcre pkgconfig fltk zlib curl openblas libsndfile fftw
+        fftwSinglePrec portaudio qrupdate arpack libwebp
+        # qt
+        # qscintilla
+        # This is commented along with --enable-jit because of:
+        # - https://savannah.gnu.org/bugs/?func=detailitem&item_id=55469
+        # - https://savannah.gnu.org/bugs/?func=detailitem&item_id=55492
+        # - https://savannah.gnu.org/bugs/?func=detailitem&item_id=46192
+        # llvm_8
+        ghostscript
+        hdf5
+        glpk
+        suitesparse
+        openjdk11
+        gnuplot
+        python37
+        libGLU_combined
+        xorg.libX11
+        # My Addition
+        sundials
+        ];
+      configureFlags =
+        [ "--enable-readline"
+          "--enable-dl"
+          "--with-blas=openblas"
+          "--with-lapack=openblas"
+          "--enable-64"
+          # "--enable-jit"
+        ];
+    });
   };
   allowUnfree = true;
 }

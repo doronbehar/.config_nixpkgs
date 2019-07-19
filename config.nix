0c16724b7711245a89d57f0d8c568628f1a8d7b6
Doron Behar <doron.behar@gmail.com>
2019-07-19 20:06:37 +0300

    Add progressbar patch for coreutils

--- config.nix
+++ config.nix
@@ -78,6 +78,17 @@
           # "--enable-jit"
         ];
     });
+    coreutils-progressbar = pkgs.coreutils.overrideAttrs(oldAttrs: rec {
+      name = "coreutils-patched";
+      patches = oldAttrs.patches ++ [(fetchpatch {
+        name = "advcpmv-0.8-8.30.patch";
+        url = "https://raw.githubusercontent.com/mrdrogdrog/advcpmv/master/advcpmv-0.8-8.30.patch";
+        sha256 = "0mw0ramg4ydqdqs33kw9m0rjvw5fvfa0scsq753pn7biwx6gx9hx";
+      })];
+      postPatch = oldAttrs.postPatch + ''
+        sed '2i echo Skipping usage vs getopt test && exit 77' -i ./tests/misc/usage_vs_getopt.sh
+      '';
+    });
   };
   allowUnfree = true;
 }

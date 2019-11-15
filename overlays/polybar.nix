63c5ce5fdec336d7949023b82d67a17448557f99
Doron Behar <doron.behar@gmail.com>
2019-11-15 11:21:55 +0200

    Update polybar

--- overlays/polybar.nix
+++ overlays/polybar.nix
@@ -3,8 +3,9 @@ self: super:
 {
   polybar = super.polybar.overrideAttrs (oldAttrs: rec {
     buildInputs = with super; [
-      cairo xorg.libXdmcp xorg.libpthreadstubs xorg.libxcb pcre xorg.xcbproto xorg.xcbutil
+      cairo xorg.libXdmcp xorg.libpthreadstubs xorg.libxcb pcre xorg.xcbutil
       xorg.xcbutilcursor xorg.xcbutilimage xorg.xcbutilrenderutil xorg.xcbutilwm xcbutilxrm
+      (xorg.xcbproto.override { python = python3; })
       alsaLib
       curl
       mpd_clientlib
@@ -12,11 +13,11 @@ self: super:
       wirelesstools
       libnl
     ];
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [super.python];
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [super.python3];
     src = self.fetchgit {
       url = "https://github.com/doronbehar/polybar.git";
-      rev = "afddd2019b6998fd7c7234168bec540fac11a03c";
-      sha256 = "0pdm7z0k6rgllxby5r2xzq9bm49h09qkm4drkr685hr52w183rxc";
+      rev = "5087f598ff09c7e8c7e087b3e89c2b0141443c2b";
+      sha256 = "07v5zfd8mx66639bbp0vb1zfnv6zwmxndxikyk2jlbk6aaw8a3cb";
       fetchSubmodules = true;
     };
     postConfigure = "";
